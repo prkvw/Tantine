ded28ac7275e4be4ce160924a8d62d6b027d725a
prkvw <neeqash@gmail.com>
2023-10-26 08:39:19 +0000

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+
+    sepolia: { url: `https://sepolia.infura.io/v3/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    
+    },
+    
+    },
+    
+  
 };
